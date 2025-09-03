951e15bc7709c5c018bb70c74c65c3b152a0bd30
RVLK <edumwID_19@hotmail.com>
2025-09-03 08:11:34 -0600

    Estilo aplicado

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 // Inicializar mapa centrado en Xalapa
 const map = L.map('map').setView([19.5438, -96.9103], 13);
 
-// Base map con estilo suave (Carto Light)
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> colaboradores &copy; <a href="https://carto.com/">CARTO</a>',
-  subdomains: 'abcd',
-  maxZoom: 20
+// Base map con Stadia Outdoors
+var Stadia_Outdoors = L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}', {
+  minZoom: 0,
+  maxZoom: 20,
+  attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  ext: 'png'
 }).addTo(map);
 
 // Marcador en el centro de Xalapa
 L.marker([19.5438, -96.9103]).addTo(map)
   .bindPopup("Centro de Xalapa")
-  .openPopup();
+  .openPopup();x
