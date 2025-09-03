d2ee082eb407bf58fca4b2e4b7f5c338c68dd688
RVLK <edumwID_19@hotmail.com>
2025-09-03 10:54:54 -0600

    SideBar a medias

--- js/index.js
+++ js/index.js
@@ -5,7 +5,6 @@ const map = L.map('map').setView([19.5438, -96.9103], 13);
 var Stadia_Outdoors = L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}', {
   minZoom: 0,
   maxZoom: 20,
-  attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   ext: 'png'
 }).addTo(map);
 
@@ -61,3 +60,11 @@ if (navigator.geolocation) {
   console.warn("Tu navegador no soporta geolocalización.");
   alert("Tu navegador no soporta geolocalización.");
 } 
+
+// Sidebar toggle
+const menuBtn = document.querySelector('.menu-btn');
+const sidebar = document.getElementById('sidebar');
+
+menuBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+});
